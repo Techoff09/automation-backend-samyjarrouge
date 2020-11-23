a23ef5ce4bf2e912cc0d2a2201d3965e62f3d076
Techoff09 <s.jarrouge@gmail.com>
2020-11-24 00:35:00 +0100

    first initialize

---
+++ backend-tests/cypress/helpers/clientHelpers.js
@@ -0,0 +1,27 @@
+const faker = require('faker')
+
+function createFakeClientPayload(){
+    const fakeName = faker.name.firstname()
+    const fakeEmail = faker.internet.email()
+    const fakePhone = faker.phone.phoneNumber()
+
+    const payload = {
+        "name":fakeName,
+        "email":fakeEmail,
+        "telephone":fakePhone
+    }
+    return payload
+}
+
+function createClientPayload(){
+    const name = 'Test'
+    const email = 'tester@email.com'
+    const 
+}
+
+module.exports = {
+     createFakeClientPayload,
+     createClientPayload,
+}
+
+
